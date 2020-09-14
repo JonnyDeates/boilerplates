2d34fb2e80d06fa9516ef99c8a8b9b77947d6d06
Jdeates <jonnydeates@gmail.com>
2020-09-14 12:37:22 -0500

    Routes

---
+++ Routes/PrivateRoute/PrivateRoute.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import {Route, Redirect} from 'react-router-dom'
+
+export default function PrivateRoute({component, ...props}) {
+
+    const Component = component;
+    const boolean = false;
+    return (
+        <Route
+            {...props}
+            render={componentProps => (
+                <>
+                    {boolean ?
+                       <Component currentActive={componentProps.location} {...componentProps} />
+                        }
+                        : (
+                            <Redirect
+                                to={{
+                                    pathname: '/',
+                                    state: {from: componentProps.location},
+                                }}
+                            />
+                        )
+
+                </>
+            )}
+        />
+    )
+}
---
+++ Routes/PublicRoute/PublicRoute.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Route } from 'react-router-dom'
+
+export default function PublicRoute({ component, ...props }) {
+  const Component = component;
+  return (
+    <Route
+      {...props}
+      render={componentProps => (
+              <Component currentActive={componentProps.location} {...componentProps} />)}
+    />
+  )
+}
